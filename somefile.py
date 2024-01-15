959d3a8ac6e6c20b0d8f1d975216f5b3bcd2f290
Ben Alexander <benjaminalexander747@gmail.com>
2024-01-15 12:22:58 +0000

    Modifiying somefile.py

---
+++ somefile.py
@@ -0,0 +1,24 @@
+def main():
+    print("Hello World!")
+    print("hi")
+
+if __name__ == "__main__":
+    main()
+
+"""
+git....
+
+add
+commit
+status
+
+push
+pull
+
+branch
+checkout (with -b)
+
+merge
+
+
+"""
